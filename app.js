0f76d0294a1a4497ffeffd84434815c4d2d0d4e5
Ankush Tripathi <89534251+ankushtripathii@users.noreply.github.com>
2023-07-01 23:47:58 +0530

    Add files via upload
    initial commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const addBtn = document.querySelector("#addbtn");
+const mainContainer = document.querySelector(".container");
+
+addBtn.addEventListener("click", function () {
+  addNote();
+});
+const saveNotes = () => {
+  //data from text area
+  const notes = document.querySelectorAll(".note textarea");
+
+  const data = [];
+  notes.forEach((note) => {
+    data.push(note.value);
+  });
+
+  if (data.length === 0) {
+    localStorage.removeItem("notes");
+  } else {
+    //storing data in localstorage
+    localStorage.setItem("notes", JSON.stringify(data));
+  }
+};
+const addNote = (text = "") => {
+  const note = document.createElement("div");
+  note.classList.add("note");
+  note.innerHTML = `
+        <div class="tools">
+          <i class="save fa-solid fa-floppy-disk"></i>
+          <i class="trash fa-solid fa-trash"></i>
+        </div>
+        <textarea>${text}</textarea>
+   `;
+  mainContainer.appendChild(note);
+  saveNotes();
+  note.querySelector(".trash").addEventListener("click", function () {
+    note.remove();
+    saveNotes();
+  });
+  note.querySelector(".save").addEventListener("click", function () {
+    saveNotes();
+  });
+
+  note.querySelector("textarea").addEventListener("focusout", saveNotes);
+};
+
+(function () {
+  const storgaeNotes = JSON.parse(localStorage.getItem("notes"));
+
+  if (storgaeNotes === null) {
+    addNote();
+  } else {
+    storgaeNotes.forEach((storgaeNotes) => {
+      addNote(storgaeNotes);
+    });
+  }
+})();
